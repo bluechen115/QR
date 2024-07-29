eb7effe759e2b04d8dd72db1b45172091e4b6ab7
bluechen115 <numberdragon01@gmail.com>
2024-07-30 00:59:39 +0900

    init

---
+++ src/app.js
@@ -0,0 +1,111 @@
+import _ from 'lodash';
+import QrCodeWithLogo from 'qrcode-with-logos'
+
+// QR Code
+let qrcodeConfg = {
+  // canvas: document.getElementById("canvas"),
+  content: 'https://google.com',
+  width: 380,
+  //   download: true,
+  image: document.getElementById('image'),
+  logo: {    
+    src: 'public/image/puppy_01.png',    
+    bgColor: 'transparent',
+    borderRadius: 5,    
+  //   borderRadius?: number;
+  //   borderColor?: string;
+  //   bgColor?: string;
+  //   crossOrigin?: string;
+  //   borderWidth?: number;
+  },
+  dotsOptions: {
+    //type: 'stripe-column', //'dot' | 'dot-small' | 'tile' | 'rounded' | 'square' | 'diamond' | 'star' | 'fluid' | 'fluid-line' | 'stripe' | 'stripe-row' | 'stripe-column'
+    color: '#000000'
+  },
+  cornersOptions: {
+    type : 'square', //'square' | 'rounded' | 'circle' | 'rounded-circle' | 'circle-rounded' | 'circle-star' | 'circle-diamond'
+    color : '#000000'
+        // radius?: number | {
+        //     inner: number;
+        //     outer: number;
+        // };
+  }
+};
+
+let qrcode = null;
+let createQrCodeWithLogo = () => {
+  qrcode = new QrCodeWithLogo(qrcodeConfg);
+}
+createQrCodeWithLogo();
+
+//HTML Input Setting
+let urlInput = document.getElementById('urlInput');
+let dotsColor = document.getElementById('dotsColor');
+let cornerColor = document.getElementById('cornerColor');
+let downloadButton = document.getElementById('downloadButton');
+let dotsType = document.getElementById('dotsType');
+let cornerType = document.getElementById('cornerType');
+let logoImage = document.getElementById('logoImage');
+let logoBgColor = document.getElementById('logoBgColor');
+let logoRadius = document.getElementById('logoRadius');
+
+urlInput.addEventListener('change', (event)=>{  
+  if(event.target.value){
+    qrcodeConfg.content = event.target.value;
+    createQrCodeWithLogo();
+  }
+})
+
+dotsColor.addEventListener('input', (event)=>{  
+  qrcodeConfg.dotsOptions.color = event.target.value;
+  createQrCodeWithLogo();
+});
+
+cornerColor.addEventListener('input', (event)=>{
+  qrcodeConfg.cornersOptions.color = event.target.value;
+  createQrCodeWithLogo();
+});
+
+downloadButton.addEventListener('click', (event)=>{
+  qrcode.downloadImage('qrcode.png')
+});
+
+dotsType.addEventListener('change', (event)=>{  
+  qrcodeConfg.dotsOptions.type = event.target.value;
+  createQrCodeWithLogo();
+});
+
+cornerType.addEventListener('change', (event)=>{  
+  qrcodeConfg.cornersOptions.type = event.target.value;
+  createQrCodeWithLogo();
+});
+
+logoImage.addEventListener('change', (event)=>{  
+  var sSrc = `public/image/${event.target.value}`;
+  qrcodeConfg.logo.src = sSrc;
+  createQrCodeWithLogo();
+});
+
+logoBgColor.addEventListener('input', (event)=>{
+  var sBgColor = event.target.value;
+  if(sBgColor=='#ffffff') {
+    sBgColor = 'transparent';
+  }
+  qrcodeConfg.logo.bgColor = sBgColor;
+  createQrCodeWithLogo();
+});
+
+logoRadius.addEventListener('change', (event)=>{
+  qrcodeConfg.logo.borderRadius = event.target.value;
+  createQrCodeWithLogo();
+});
+
+//qrcode.downloadImage('qrcode.png')
+
+// function component() {
+//   const element = document.createElement('div');
+//   element.innerHTML = _.join(['Hello', 'Moon'], ' ');
+//   return element;
+// }
+
+// document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, 'dist'),
+      },
+      {
+        directory: path.join(__dirname, 'public'),
+        publicPath: '/public'
+      },
+    ],
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
